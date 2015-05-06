36ccc5d309d73c23d1c865923f392c86088673c4
chydziex <mateusz.chydzinski@onet.eu>
2015-05-06 16:45:13 +0200

    Dodalem szkic funkcji nasluchujacej na zapytania o dostepnosc zasobu i maly schemat, ktory socket komunikuje sie z ktorym

--- main2.c
+++ main2.c
@@ -6,11 +6,18 @@
 #include <sys/time.h>
 
 #define SIZEDATAGRAM 512
+#define LISTENPORT 5555
+
+#define RESOURCE_FOUND 255
+#define RESOURCE_NOT_FOUND 0
 
 const char* directoryPath; // katalog z udostep. plikami
 int sendfile(FILE* file, struct sockaddr *to);
+
+
 int main(int argc, char* argv[])
 {
+	
 	char *command = NULL; // komenda podawana przez uzytkownika
 	char *ptr; // wsk na nazwe pliku 
 	int sizecommand; // rozmiar komendy ( nieuzywany - tylko do wywolania getline)
@@ -162,8 +169,142 @@ int sendfile(FILE* file, struct sockaddr *to)
 
 }
 
+FILE* findResource(char *fileName)
+{
+	FILE *fp;
+	char fname[261] = "/home/sebastian/Pulpit/TIN/";
+	strncat(fname, fileName, 250);
+	if ((fp = fopen(fname, "rb")) == NULL)
+	{
+		printf("Nie moge otworzyc takiego pliku!\n");
+		return NULL;
+	}
+	return fp;
+}
 
 
 
+void listen()
+{
 
+	//bufor na dane z gniazda nasluchujacego
+	char * listenBuffer = malloc(SIZEDATAGRAM);
 
+	//utworzenie gniazda nasluchujacego na pytania o zasoby
+	int listenSock;
+	listenSock = socket(AF_INET, SOCK_DGRAM, 0); // utworzenie gniazda
+
+	//przypisanie adresu i portu do gniazda
+	struct sockaddr_in listenAddr;
+	listenAddr.sin_family = AF_INET;
+	listenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	listenAddr.sin_port = htons(LISTENPORT);
+
+	if (bind(listenSock, (const struct sockaddr *) &listenAddr, sizeof(struct sockaddr)) != 0){
+		printf("blad funkcji bind listenSock");
+		return 0;
+	}
+
+	//struktura do zapisania adresu i portu, skad przyszlo zapytanie o plik
+	struct sockaddr_in sourceAddr;
+	socklen_t sourceAddrLenght;
+
+	while (true)
+	{
+		//odbiera zapytanie o dany plik, zapisuje do struktury adres i port wezla, ktory pytal
+		if (recvfrom(listenSock, listenBuffer, SIZEDATAGRAM, 0, &sourceAddr, &sourceAddrLenght) < 0)
+		{
+			printf("blad recvfrom listen");
+			return 0;
+		}
+
+		//tworzymy proces do obslugi wyszukiwania zasobu i ewentulnego transferu zasobu
+		if (fork() == 0)
+		{
+			//kopiowanie adresu do obslugi z rodzica do dziecka...
+			//POTRZEBNA SYNCHRONIZACJA - CO JESLI PRZYJDZIE KOLEJNE ZAPYTANIE DO RODZICA, JESLI DZIECKO NIE ZDAZY SKOPIOWAC TEJ STRUKTURY Z ADRESEM?
+			//							- OBSLUZYMY NIE TEGO KLIENTA KTOREGO TRZEBA :D
+			struct sockaddr_in sendAddr;
+			socklen_t sendAddrLenght;
+			sendAddrLength = sourceAddrLength;
+			memcpy(&sendAddr, &sourceAddr, sendAddrLength);
+
+			//kopiowanie nazwy pliku z rodzica do dziecka
+			//POTRZEBNA SYNCHRONIZACJA - JAK WYZEJ
+			char * fileName = malloc(SIZEDATAGRAM);
+			memcpy(&fileName, &listenBuffer);
+
+			//chyba tego nie potrzebujemy w tym procesie, mozemy zamknac?
+			close(listenSock);
+
+			//bufor na odpowiedz czy jest zasob czy nie
+			char * respBuffer = malloc(SIZEDATAGRAM);
+
+			//utworzenie gniazda obslugujacego wyszukanie i ewentualny transfer zasobu
+			int serviceSock;
+			serviceSock = socket(AF_INET, SOCK_DGRAM, 0); // utworzenie gniazda
+
+			//przypisanie adresu i portu do gniazda
+			struct sockaddr_in serviceAddr;
+			listenAddr.sin_family = AF_INET;
+			listenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+			listenAddr.sin_port = htons(0);
+
+			if (bind(serviceSock, (const struct sockaddr *) &serviceAddr, serviceAddrLength) != 0)
+			{
+				printf("blad funkcji bind serviceSock");
+				exit(0);
+			}
+
+			//szukamy zasobu
+			FILE* file = findResource(fileName);
+
+			//jest
+			if (file != NULL)
+			{
+				respBuffer[0] = RESOURCE_FOUND;
+
+				//wyslanie odpowiedzi, ze zasob jest
+				//WYSYLAMY Z NOWEGO GNIAZDA - WEZEL PYTAJACY DOSTANIE NASZ NOWY ADRES I NOWY PORT
+				sendto(serviceSock, respBuffer, sizeof(char), 0, &sendAddr, sizeof(sendAddr));
+
+				//czekanie na potwierdzenie transferu zasobu
+				//WEZEL PYTAJACY UTWORZYL NOWE GNIAZDO
+				//ODBIERAMY ODPOWIEDZ Z NOWEGO ADRESU I PORTU - zapisujemy go do sendAddr i sendAddrLength
+				if (recvfrom(serviceSock, respBuffer, SIZEDATAGRAM, 0, &sendAddr, &sendAddrLenght) < 0)
+				{
+					printf("blad recvfrom listen");
+					exit(0);
+				}
+
+				//wysylanie pliku
+				//WYSYLAMY POD NOWY ADRES I PORT, KTORY OTRZYMALISMY PRZED CHWILA
+				sendfile(file, &sendAddr);
+				fclose(file);
+			}
+			else
+				//nie ma
+			{
+				respBuffer[0] = RESOURCE_NOT_FOUND;
+
+				//wyslanie odpowiedzi, ze zasobu nie ma
+				//WYSYLAMY Z NOWEGO GNIAZDA - WEZEL PYTAJACY DOSTANIE NASZ NOWY ADRES I NOWY PORT
+				//PYTAJACY NIE UTWORZYL NOWEGO GNIAZDA
+				//WYSYLAMY POD STARY ADRES I PORT, Z KTOREGO PRZYSZLO ZAPYTANIE O PLIK
+				sendto(serviceSock, respBuffer, sizeof(char), 0, &sendAddr, sizeof(sendAddr));
+			}
+
+			close(serviceSock);
+
+			//koniec potomka
+			exit(0);
+		}
+
+		//rodzic
+		else
+		{
+			//chyba nic nie robimy - przechodzimy do nasluchiwania na kolejne zapytanie o zasob
+
+		}
+	}
+}
